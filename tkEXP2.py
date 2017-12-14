f97396bba33647df6ae7b72ad8411c6bb56a4d5f
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2017-12-14 08:57:09 -0600

    added testing code

---
+++ tkEXP2.py
@@ -0,0 +1,33 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 22 2016, 17:23:13) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+import tkinter as tk
+>>> class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+        
+>>> def start():
+	root = tk.Tk()
+	app = Application(master=root)
+	app.mainloop()
+
+	
+>>> 
