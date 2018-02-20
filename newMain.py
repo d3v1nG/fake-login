313a3f11edc75308dd96e961dac7d6b5bacce276
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2018-02-19 22:49:27 -0600

    Added entry widget

--- newMain.py
+++ newMain.py
@@ -1,20 +1,36 @@
-from Tkinter import *
-import Tkinter as tk
+
+#for 3.6.4
+from tkinter import *
+import tkinter as tk
 
 class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
+        self.grid()
         self.widgets()
+        self.entry()
         self.master.title("Finder wants to make changes.")
 
+#Button functions (this dont work yet)
+    def okay():
+        print(E1.get())
+
+#Widgets and Entries
     def widgets(self):
-        okayButton = Button(self, text="Okay")
-        okayButton.grid(row=1, column=1)
+        okayButton = Button(self, text="Okay", command=okay())
+        okayButton.grid(row=5, column=1)
 
+        cancelButton = Button(self, text="Cancel")
+        cancelButton.grid(row=5, column=2)
 
+    def entry(self):
+        L1 = Label(self, text="User Name")
+        L1.grid(row=2, column=1)
+        E1 = Entry(self, bd =5)
+        E1.grid(row=2, column=2)
 
-
+#Starts the program
 def start():
     root = Tk()
     root.grid()
