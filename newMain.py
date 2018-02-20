edb8c5376de37e7f686f2807a7df1c2a2edc95f8
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2018-02-20 09:59:01 -0600

    Update newMain.py

--- newMain.py
+++ newMain.py
@@ -1,7 +1,10 @@
+#for 2.7.10
+from Tkinter import *
+#import Tkinter as Tk
 
 #for 3.6.4
-from tkinter import *
-import tkinter as tk
+#from tkinter import *
+#import tkinter as tk
 
 class Window(Frame):
     def __init__(self, master=None):
@@ -13,28 +16,33 @@ class Window(Frame):
         self.master.title("Finder wants to make changes.")
 
 #Button functions (this dont work yet)
-    def okay():
-        print(E1.get())
+    def okay(E1):
+        tries = 0
+        user = str(var.get())
+        print("User Name: " + user)
+
+
+    def exitButton(self):
+        exit()
 
 #Widgets and Entries
     def widgets(self):
-        okayButton = Button(self, text="Okay", command=okay())
+        okayButton = Button(self, text="Okay", command=self.okay)
         okayButton.grid(row=5, column=1)
 
-        cancelButton = Button(self, text="Cancel")
+        cancelButton = Button(self, text="Cancel", command=self.exitButton)
         cancelButton.grid(row=5, column=2)
 
     def entry(self):
         L1 = Label(self, text="User Name")
         L1.grid(row=2, column=1)
-        E1 = Entry(self, bd =5)
+        E1 = Entry(self, bd =5, textvariable=var)
         E1.grid(row=2, column=2)
 
 #Starts the program
 def start():
-    root = Tk()
-    root.grid()
-    app = Window(root)
-    root.mainloop()
+    master = Tk()
+    app = Window(master)
+    master.mainloop()
 
 start()
