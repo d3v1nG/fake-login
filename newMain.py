9ff6f542c71218448dfbda42312b65ae91555bb2
d3v1nG <33507591+d3v1nG@users.noreply.github.com>
2018-02-28 23:25:42 -0600

    window looks good, tries and bind

--- newMain.py
+++ newMain.py
@@ -8,6 +8,9 @@ class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
+        master.resizable(0,0)
+        master.pack_propagate(0)
+        master.geometry("500x200")
         self.grid()
         self.widgets()
         self.entry()
@@ -18,7 +21,7 @@ class Window(Frame):
     def finderImg(self):
         #Image doesnt want to fucking show up.
         canvas = Canvas(self, width = 100, height = 93)
-        canvas.grid(row=2, column=1)
+        canvas.grid(row=2, column=1, rowspan=2)
 
         img = PhotoImage(file="./Images/finder.gif")
         canvas.create_image(50, 50, image=img)
@@ -26,48 +29,57 @@ class Window(Frame):
 
     def widgets(self):
         okayButton = Button(self, text="Okay", command=self.okay)
-        okayButton.grid(row=6, column=3)
+        okayButton.grid(row=7, column=4)
 
         cancelButton = Button(self, text="Cancel", command=self.exitButton)
-        cancelButton.grid(row=6, column=2)
+        cancelButton.grid(row=7, column=3)
 
     def entry(self):
         titleLabel = Label(self, text="Finder wants to make changes.")
-        titleLabel.grid(row=1, column=2, columnspan=2)
+        titleLabel.grid(row=1, column=2, columnspan=2, sticky=W)
+
+        head = Label(self, text="Enter an administrator's name and password to allow this.")
+        head.grid(row=2, column=2, columnspan=2)
 
         L1 = Label(self, text="User Name: ")
-        L1.grid(row=2, column=2)
+        L1.grid(row=3, column=2, sticky=E)
         self.username = tk.Entry(self)
-        self.username.grid(row=2, column=3)
+        self.username.grid(row=3, column=3)
         self.username.bind('<Return>', self.okay)
 
         L2 = Label(self, text="Password: ")
-        L2.grid(row=3, column=2)
+        L2.grid(row=4, column=2, sticky=E)
         bullet = "\u2022"
         self.password = tk.Entry(self)
-        self.password.grid(row=3, column=3)
+        self.password.grid(row=4, column=3)
         self.password.config(show=bullet)
         self.password.bind('<Return>', self.okay)
 
 #Button functions (works, but not with <Return> bind)
 # tries idea not trashed, work on later
     def okay(self):
-        for tries in range(3):
-            user = self.username.get()
-            password = self.password.get()
-            if user == "" or password == "":
-                print("No username or password entered.")
-            else:
-                print("User Name: " + user)
-                print("Password: " + password)
-        if int(tries) == 3:
-            exitButton(self)
+        self.tryLoop()
+        user = self.username.get()
+        password = self.password.get()
+        if user == "" or password == "":
+            print("No username or password entered.")
         else:
-            print("Try: "+ str(tries))
+            print("User Name: " + user)
+            print("Password: " + password)
 
     def exitButton(self):
         print("~Exiting~")
         exit()
+
+#the "incorrect password" loop
+    def tryLoop(self):
+        tries = 0
+        if tries == 3:
+            exitButton()
+        else:
+            tries = tries + 1
+            return tries
+            print("~Input number " + str(tries) + "~")
 
 #Starts the program
 def start():
